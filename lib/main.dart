5203e0f239f54383062b3f0df9081a62b16bc6a5
Juan Carlos <jcguitar1994@gmail.com>
2020-04-01 08:31:45 -0300

    Main file

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/favorite_bloc.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
+import 'package:fluttertube/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      blocs: [
+        Bloc((i) => VideosBloc()),
+        Bloc((i) => FavoriteBloc()),
+      ],
+      child: MaterialApp(
+        title: 'FlutterTube',
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
+    );
+  }
+}
